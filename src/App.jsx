6749144e3df9eda44ea05a6597b9d5ee0c26e3af
solomon-drn <sdgmasterpiece@gmail.com>
2025-12-26 17:36:19 +0000

    moved orders and tracking pages to react

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from "react-router";
 import HomePage from "./pages/HomePage.jsx";
 import CheckoutPage from "./pages/CheckoutPage.jsx";
+import OrdersPage from "./pages/OrdersPage.jsx";
+import TrackingPage from "./pages/TrackingPage.jsx";
 
 function App() {
   return (
     <Routes>
       <Route index element={<HomePage/>} />
-      <Route path="checkout" element={<CheckoutPage/>} />
+      <Route path="/checkout" element={<CheckoutPage/>} />
+      <Route path="/orders" element={<OrdersPage/>} />
+      <Route path="/tracking" element={<TrackingPage/>} />
     </Routes>
   );
 }
