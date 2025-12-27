51f8ff118b2eaf69887dc7cadff43607aeaf0622
solomon-drn <sdgmasterpiece@gmail.com>
2025-12-27 09:19:30 +0000

    created 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import HomePage from "./pages/HomePage.jsx";
 import CheckoutPage from "./pages/Checkout/CheckoutPage.jsx";
 import OrdersPage from "./pages/OrdersPage.jsx";
 import TrackingPage from "./pages/TrackingPage.jsx";
+import NotFound from "./pages/NotFound.jsx";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path="/checkout" element={<CheckoutPage/>} />
       <Route path="/orders" element={<OrdersPage/>} />
       <Route path="/tracking" element={<TrackingPage/>} />
+      <Route path="*" element={<NotFound/>} />
     </Routes>
   );
 }
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,15 @@
+import Header from "../components/Header";
+import "./NotFound.css";
+
+const NotFound = () => {
+  return (
+    <>
+      <Header />
+      <div className="not-found">
+        <p>Page not found</p>
+      </div>
+    </>
+  );
+};
+
+export default NotFound;
