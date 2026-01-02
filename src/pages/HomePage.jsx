809c5ca523f192c2b0b8339079aa1bf627d0491b
solomon-drn <sdgmasterpiece@gmail.com>
2026-01-02 21:53:01 +0000

    add server proxy config for /api and /images

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,11 +9,11 @@ const HomePage = () => {
   const [cart, setCart] = useState([])
 
   useEffect(() => {
-    axios.get("http://localhost:3000/api/products").then((response) => {
+    axios.get("/api/products").then((response) => {
       setProducts(response.data);
     });
 
-    axios.get("http://localhost:3000/api/cart-items")
+    axios.get("/api/cart-items")
       .then(response => {
       setCart(response.data)
     })
